217aed85530e6f03ea7b19403aa5edb88b93bfab
Chaggai <chaggaith@gmail.com>
2021-05-03 09:52:29 +0300

    initialCommit

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './App.module.css';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  return <div className={classes.App}></div>;
+}
+
+export default App;
